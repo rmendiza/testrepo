b8b625094bf7b5f546426781dd1e486e00718261
Sunitha Muthukrishna <sumuth@microsoft.com>
2020-10-12 20:00:09 -0700

    Update employeedb.sql

--- employeedb.sql
+++ employeedb.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employees (
-   employee_id   NUMERIC       NOT NULL,
+   employeeid   NUMERIC       NOT NULL,
    first_name    VARCHAR(1000) NOT NULL,
    last_name     VARCHAR(1000) NOT NULL,
    date_of_birth DATE                   ,
