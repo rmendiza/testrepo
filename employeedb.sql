2940e770bb9b2e15f2dc3d134a67f6df40e83c33
rmendiza <134438670+rmendiza@users.noreply.github.com>
2023-09-29 15:22:31 -0700

    Update employeedb.sql

--- employeedb.sql
+++ employeedb.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employees (
-   employeeid   NUMERIC       NOT NULL,
+   employee_id   NUMERIC       NOT NULL,
    first_name    VARCHAR(1000) NOT NULL,
    last_name     VARCHAR(1000) NOT NULL,
    date_of_birth DATE                   ,
