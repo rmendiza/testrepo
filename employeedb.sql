f290bd0c574244ede31f31d684b58ecd58bb3317
rmendiza <134438670+rmendiza@users.noreply.github.com>
2023-09-29 16:37:54 -0700

    Update employeedb.sql

--- employeedb.sql
+++ employeedb.sql
@@ -1,12 +1,18 @@
-CREATE TABLE employees (
-   employee_id   NUMERIC       NOT NULL,
-   first_name    VARCHAR(1000) NOT NULL,
-   last_name     VARCHAR(1000) NOT NULL,
-   date_of_birth DATE                   ,
-   phone_number  VARCHAR(1000) NOT NULL,
-   junk          CHAR(1000)             ,
-   CONSTRAINT employees_pk PRIMARY KEY (employee_id)
-);
+-- Check if the table already exists
+IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'employees') THEN
+  -- Create the table if it does not exist
+  CREATE TABLE employees (
+     employee_id   NUMERIC       NOT NULL,
+     first_name    VARCHAR(1000) NOT NULL,
+     last_name     VARCHAR(1000) NOT NULL,
+     date_of_birth DATE                   ,
+     phone_number  VARCHAR(1000) NOT NULL,
+     junk          CHAR(1000)             ,
+     last_modified TIMESTAMP              , -- Add a last_modified column
+     CONSTRAINT employees_pk PRIMARY KEY (employee_id)
+  );
+END IF;
+
 CREATE FUNCTION random_string(minlen NUMERIC, maxlen NUMERIC)
 RETURNS VARCHAR(1000)
 AS
@@ -30,16 +36,18 @@ END;
 $$ LANGUAGE plpgsql;
 INSERT INTO employees (employee_id,  first_name,
                        last_name,    date_of_birth, 
-                       phone_number, junk)
+                       phone_number, junk, last_modified)
 SELECT GENERATE_SERIES
      , initcap(lower(random_string(2, 8)))
      , initcap(lower(random_string(2, 8)))
      , CURRENT_DATE - CAST(floor(random() * 365 * 10 + 40 * 365) AS NUMERIC) * INTERVAL '1 DAY'
      , CAST(floor(random() * 9000 + 1000) AS NUMERIC)
      , 'junk'
+     , CURRENT_TIMESTAMP -- Set the last_modified column to the current timestamp
   FROM GENERATE_SERIES(1, 30);
 UPDATE employees 
    SET first_name='MARKUS', 
-       last_name='WINAND'
+       last_name='WINAND',
+       last_modified = CURRENT_TIMESTAMP -- Update the last_modified column when changing other columns
  WHERE employee_id=123;
 VACUUM ANALYZE employees;
